836e88731e06751ad3bc2282b8db8efa88d92e02
amilovanovikj <andrejmilovanovic98@gmail.com>
2021-03-20 00:33:39 +0100

    AB#47 Update Jyputer book version 0.6.4 -> 0.10.1

---
+++ intro.md
@@ -0,0 +1,7 @@
+<img src="images/mrm_v84_6.gif" style="width:114px;height:auto;margin: 0 20px;"  align="right">
+
+# Introduction
+
+Welcome to an interactive Jupyter Book for the Magnetic Resonance in Medicine paper by Michael Loecher et al. entitled "[*A gradient optimization toolbox for general purpose time‐optimal MRI gradient waveform design*](https://onlinelibrary.wiley.com/doi/full/10.1002/mrm.28384)".
+
+This Jupyter Book lets you interact with a [demo of their code](https://github.com/cmr-group/gropt) to test their gradient optimization toolbox. You can change the code by inline on the pages or launch a MyBinder session to run your code in a Jupyter Notebook in another tab. The figures are made interactive using [Plotly](https://plotly.com).
---
+++ python/interactive_plots.py
@@ -0,0 +1,162 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import chart_studio.plotly as py
+import cufflinks as cf
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+from helper_utils import *
+
+
+def plot_waveform_interactive(G, params, plot_moments = True, plot_eddy = True, plot_pns = True, plot_slew = True,
+                  suptitle = '', eddy_lines=[], eddy_range = [1e-3,120,1000], width=900, height=550):
+    
+    n_axis = params.get('Naxis', 1)
+    num_traces = n_axis + int(plot_slew)*n_axis + int(plot_moments)*3 + int(plot_pns) + int(plot_eddy)
+    current_trace = 0
+
+    TE = params['TE']
+    T_readout = params['T_readout']
+    diffmode = 0
+    if params['mode'][:4] == 'diff':
+        diffmode = 1
+    
+    dt = (TE-T_readout) * 1.0e-3 / G.shape[1]
+    tt = np.arange(G.shape[1]) * dt * 1e3
+    tINV = TE/2.0
+    
+    bval = get_bval(G, params)
+    blabel = 'b-value=%.0f' % bval # TODO: should add "$mm^{2}/s$"
+    
+    fig_title = None
+    if suptitle:
+        fig_title = suptitle + ': ' + blabel
+    elif diffmode > 0:
+        fig_title = blabel
+        
+    fig = go.Figure(layout=dict(xaxis=dict(title="$t [ms]$")))
+    buttons = []
+    
+    # Gradient
+    for i in range(n_axis):
+        fig.add_trace(go.Scatter(x=tt, y=G[i]*1000, showlegend=False))
+    buttons.append(dict(label = "Gradient",
+                   method = "update",
+                   args = [{"visible": [True if x>= current_trace and x < current_trace + n_axis
+                                        else False for x in range(num_traces)],
+                           "showlegend":False},
+                           {"shapes":[],
+                           "xaxis": {"title": "$t [ms]$"}}]))
+    current_trace += n_axis
+    
+    # Slew
+    if plot_slew:
+        for i in range(n_axis):
+            fig.add_trace(go.Scatter(x=tt[:-1], y=np.diff(G[i])/dt, visible=False, showlegend=False))
+        buttons.append(dict(label = "Slew",
+                       method = "update",
+                       args = [{"visible": [True if x>= current_trace and x < current_trace + n_axis
+                                            else False for x in range(num_traces)],
+                               "showlegend":False},
+                               {"shapes":[],
+                               "xaxis": {"title": "$t [ms]$"}}]))
+        current_trace += n_axis
+            
+    # Moments
+    if plot_moments:
+        moment_lines = []
+        moment_lines.append({"type":"line", "x0":tt[0], "x1":tt[-1], "y0":0, "y1":0,
+                          "line":{"color":"#777777", "width":1, "dash":"dash"}})
+        mm = get_moment_plots(G, T_readout, dt, diffmode)
+        for i in range(3):
+            if diffmode == 1:
+                mmt = mm[i]/np.abs(mm[i]).max()
+            if diffmode == 0:   
+                if i == 0:
+                    mmt = mm[i]*1e6
+                if i == 1:
+                    mmt = mm[i]*1e9
+                if i == 2:
+                    mmt = mm[i]*1e12
+            fig.add_trace(go.Scatter(x=tt, y=mmt, visible=False, name='$M_{%d}$'%i))
+        buttons.append(dict(label = "Moments",
+                       method = "update",
+                       args = [{"visible": [True if x>= current_trace and x < current_trace + 3
+                                            else False for x in range(num_traces)],
+                               "showlegend":True},
+                               {"shapes":moment_lines,
+                               "xaxis": {"title": "$Time [ms]$"}}]))
+        current_trace += 3
+            
+    # Eddy
+    if plot_eddy:
+        all_lam = np.linspace(eddy_range[0],eddy_range[1],eddy_range[2])
+        all_e = []
+        for lam in all_lam:
+            lam = lam * 1.0e-3
+            r = np.diff(np.exp(-np.arange(G[0].size+1)*dt/lam))[::-1] # TODO: 3-axis case, right now just assumes 1 axis
+            all_e.append(100*r@G[0])
+        fig.add_trace(go.Scatter(x=all_lam, y=all_e, visible=False, showlegend=False))
+
+        eddy_draw = []
+        for e in eddy_lines:
+            min_e = min(all_e)
+            max_e = max(all_e)
+            amp = 0.1*max(abs(min_e), abs(max_e))
+            eddy_draw.append(dict(type="line",
+                x0=e, x1=e, y0=min(all_e)-amp, y1=max(all_e)+amp,
+                line=dict(color="#D64B4B",
+                    width=1,
+                    dash="dot")))
+        eddy_draw.append({"type":"line", "x0":all_lam[0], "x1":all_lam[-1], "y0":0, "y1":0,
+                          "line":{"color":"#777777", "width":1, "dash":"dash"}})
+        buttons.append(dict(label = "Eddy",
+                       method = "update",
+                       args = [{"visible": [True if x>= current_trace and x < current_trace + 1
+                                            else False for x in range(num_traces)],
+                               "showlegend":False},
+                               {"shapes":eddy_draw,
+                               'xaxis': {'title': "$\lambda [ms]$"}}]))
+        current_trace += 1
+    
+    # PNS
+    if plot_pns:
+        pns_lines = []
+        pns_lines.append({"type":"line", "x0":tt[0], "x1":tt[-2], "y0":1, "y1":1,
+                      "line":{"color":"#D64B4B", "width":1, "dash":"dot"}})
+        pns_lines.append({"type":"line", "x0":tt[0], "x1":tt[-2], "y0":0, "y1":0,
+                          "line":{"color":"#777777", "width":1, "dash":"dash"}})
+        pns = np.abs(get_stim(G, dt))
+        fig.add_trace(go.Scatter(x=tt[:-1], y=pns, visible=False, showlegend=False))
+        buttons.append(dict(label = "PNS",
+                       method = "update",
+                       args = [{"visible": [True if x>= current_trace and x < current_trace + 1
+                                            else False for x in range(num_traces)],
+                               "showlegend":False},
+                               {"shapes":pns_lines,
+                               "xaxis": {"title": "$Time [ms]$"}}]))
+        current_trace += 1
+        
+    # Layout
+    update_menu = [
+        dict(active = 0,
+            direction="down",
+            pad={"r": 10, "t": 10},
+            showactive=True,
+            x=1.025,
+            xanchor="left",
+            y=.825,
+            yanchor="top",
+            buttons=buttons
+            )]
+    
+    fig.update_layout(title=fig_title,
+        updatemenus=update_menu,
+        width=width,
+        height=height,
+        autosize=False,
+        margin=dict(t=50, b=50, l=0, r=50),
+        template="plotly_white")
+    
+    return fig
